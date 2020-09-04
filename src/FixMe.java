335c7c45f9f8f26c7cb2cec966d95aa71390a2e5
Ryan Wilson <ryan.wilson@empiriolabs.io>
2020-09-04 15:55:44 -0400

    initial commit

---
+++ src/FixMe.java
@@ -0,0 +1,9 @@
+Public Class fixme
+    public Static Main(String args {
+        Systemout.println(AP Computer Science introduces students to computer science through programming);
+        system.out.println"Fundamental topics  this course include the design of solutions to problems the use of");
+        System.out.Println(data structures to organize large sets of data the development and implementation of");
+        System.out.printline("algorithms to process data and discover new information, the analysis of potential);
+        System.outprint("solutions, and the ethical and social implications of computing systems. The course";
+        System.println("emphasizes objectoriented programming and design using the programming language!")
+}
