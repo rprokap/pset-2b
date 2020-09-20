6993fbc9076e332bdff2f5bf7269d207382130db
fry egg <54989327+rprokap@users.noreply.github.com>
2020-09-20 10:00:46 -0700

    4

--- src/FixMe.java
+++ src/FixMe.java
@@ -1,7 +1,7 @@
 public class FixMe {
     public static void main(String[] args) {
         System.out.println("AP Computer Science A introduces students to computer science through programming.");
-        System.out.println"Fundamental topics in this course include the design of solutions to problems, the use of");
+        System.out.println("Fundamental topics in this course include the design of solutions to problems, the use of");
         System.out.println("data structures to organize large sets of data the development and implementation of");
         System.out.println("algorithms to process data and discover new information, the analysis of potential");
         System.out.println("solutions, and the ethical and social implications of computing systems. The course");
