8dc26424a9fe90ce690c6bb6dc376694e2d4b274
fry egg <54989327+rprokap@users.noreply.github.com>
2020-09-20 09:40:08 -0700

    i try to fixtm

--- src/FixMe.java
+++ src/FixMe.java
@@ -1,9 +1,12 @@
-Public Class fixme
-    public Static Main(String args {
-        Systemout.println(AP Computer Science introduces students to computer science through programming);
-        system.out.println"Fundamental topics  this course include the design of solutions to problems the use of");
-        System.out.Println(data structures to organize large sets of data the development and implementation of");
+public class FixMe {
+    public static void main(String[] args) {
+        Systemout.println("AP Computer Science A introduces students to computer science through programming.");
+        system.out.println"Fundamental topics in this course include the design of solutions to problems, the use of");
+        System.out.Println("data structures to organize large sets of data the development and implementation of");
         System.out.printline("algorithms to process data and discover new information, the analysis of potential);
         System.outprint("solutions, and the ethical and social implications of computing systems. The course";
-        System.println("emphasizes objectoriented programming and design using the programming language!")
+        System.println("emphasizes object-oriented programming and design using the Java programming language.");
 }
+}
+                        
+                
