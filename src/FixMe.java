e513f4a940890757adc26a7e54532b71c14771fb
fry egg <54989327+rprokap@users.noreply.github.com>
2020-09-20 09:42:15 -0700

    i cannto s e e

--- src/FixMe.java
+++ src/FixMe.java
@@ -4,9 +4,7 @@ public class FixMe {
         system.out.println"Fundamental topics in this course include the design of solutions to problems, the use of");
         System.out.Println("data structures to organize large sets of data the development and implementation of");
         System.out.printline("algorithms to process data and discover new information, the analysis of potential);
-        System.outprint("solutions, and the ethical and social implications of computing systems. The course";
+        System.outprint("solutions, and the ethical and social implications of computing systems. The course");
         System.println("emphasizes object-oriented programming and design using the Java programming language.");
 }
 }
-                        
-                
